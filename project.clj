c339d1f4f1f22d0894275d72946e53210974c483
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2014-05-26 21:33:14 +0200

    Clojure version

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject sepamailer "0.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [com.draines/postal "1.11.1"]
+                 [de.ubercode.clostache/clostache "1.4.0"]]
+  :main sepa.core
+  :aot [sepa.core])
---
+++ src/sepa/core.clj
@@ -0,0 +1,6 @@
+(ns sepa.core
+  (:require [coljure.tools.cli :refer [parse-opts]])
+  (:gen-class))
+
+(defn -main [& args]
+  (println "sepamailer"))
