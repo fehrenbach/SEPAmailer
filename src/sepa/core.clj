d3fb961fc2c2f880d635eefd00889ac517669405
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2014-05-27 11:23:11 +0200

    Note

--- src/sepa/core.clj
+++ src/sepa/core.clj
@@ -1,6 +1,9 @@
 (ns sepa.core
-  (:require [coljure.tools.cli :refer [parse-opts]])
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
 (defn -main [& args]
   (println "sepamailer"))
+
+;; Guess mime type
+;; http://docs.oracle.com/javase/7/docs/api/java/nio/file/spi/FileTypeDetector.html
